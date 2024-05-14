5efbbe20a42f4b4de37f2ae94e2b4d68f67fa846
tsomic <keller.nils99@gmail.com>
2024-05-15 01:28:50 +0200

    🧑‍💻 add placeholder get_images

--- led_matrix_application/api.py
+++ led_matrix_application/api.py
@@ -5,7 +5,7 @@ from database import Database
 
 db = Database()
 
-async def get_state():
+async def get_state(request):
     state = db.get_state()
     return JSONResponse(state)
 
@@ -14,7 +14,13 @@ async def patch_state(request):
     db.patch_state(state)
     return JSONResponse(db.get_state())
 
+async def get_images(request):
+    return JSONResponse({"images":[
+        "http://example.com/image.png"
+    ]})
+
 routes = [
     Route("/state", endpoint=get_state, methods=["GET"]),
     Route("/state", endpoint=patch_state, methods=["PATCH"]),
+    Route("/images", endpoint=get_images, methods=["GET"]),
 ]
