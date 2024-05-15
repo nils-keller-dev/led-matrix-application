350ab166a067be05cb6a8dee252c07300b9927f6
tsomic <keller.nils99@gmail.com>
2024-05-15 23:16:14 +0200

    🚧 add placeholder image to music mode

--- led_matrix_application/mode/music_mode.py
+++ led_matrix_application/mode/music_mode.py
@@ -1,12 +1,20 @@
+import time
+
 from mode.abstract_mode import AbstractMode
+from PIL import Image
 
 
 class MusicMode(AbstractMode):
+    def __init__(self, matrix):
+        super().__init__(matrix)
+        self.logo = Image.open("icons/spotify.png")
+
     def start(self):
+        self.matrix.Clear()
+        self.matrix.SetImage(self.logo, 20, 20, False)
+
+    def update_settings(self, _):
         pass
 
-    def update_settings(self, settings):
-        self.settings = settings
-
     def update_display(self):
-        self.matrix.Fill(0, 0, 255)
+        time.sleep(0.1)
