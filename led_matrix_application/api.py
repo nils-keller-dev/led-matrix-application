e6e4ec8b870c9a7494f0d0e574eb37e264b8b8b7
tsomic <keller.nils99@gmail.com>
2024-05-15 21:43:34 +0200

    ✨ add basic image mode

--- led_matrix_application/api.py
+++ led_matrix_application/api.py
@@ -1,5 +1,7 @@
+import os
+
 from database import Database
-from starlette.responses import JSONResponse
+from starlette.responses import FileResponse, JSONResponse
 from starlette.routing import Route
 
 db = Database()
@@ -17,11 +19,19 @@ async def patch_state(request):
 
 
 async def get_images(_):
-    return JSONResponse({"images": ["http://example.com/image.png"]})
+    stripped_list = []
+    for file in os.listdir("images"):
+        stripped_list.append(file)
+    return JSONResponse({"images": stripped_list})
+
+
+async def get_image(request):
+    return FileResponse(f"images/{request.path_params['image']}")
 
 
 routes = [
     Route("/state", endpoint=get_state, methods=["GET"]),
     Route("/state", endpoint=patch_state, methods=["PATCH"]),
     Route("/images", endpoint=get_images, methods=["GET"]),
+    Route("/image/{image}", endpoint=get_image, methods=["GET"]),
 ]
--- led_matrix_application/mode/image_mode.py
+++ led_matrix_application/mode/image_mode.py
@@ -1,12 +1,23 @@
+import time
+
 from mode.abstract_mode import AbstractMode
+from PIL import Image
 
 
 class ImageMode(AbstractMode):
+    def __init__(self, matrix):
+        super().__init__(matrix)
+        self.offscreen_canvas = matrix.CreateFrameCanvas()
+
     def start(self):
-        pass
+        self.matrix.Clear()
 
     def update_settings(self, settings):
-        self.settings = settings
+        img = Image.open(f"images/{settings['image']}")
+        img.thumbnail((64, 64))
+        offset = (64 - img.size[0]) // 2, (64 - img.size[1]) // 2
+        self.matrix.Clear()
+        self.matrix.SetImage(img.convert("RGB"), offset[0], offset[1], False)
 
     def update_display(self):
-        self.matrix.Fill(0, 255, 0)
+        time.sleep(0.1)
