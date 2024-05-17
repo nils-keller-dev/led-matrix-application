68f7aef9e539185f49cbb12fd0efa1805ae3c472
tsomic <keller.nils99@gmail.com>
2024-05-17 21:15:17 +0200

    🚧 implement WIP text mode

--- led_matrix_application/mode/text_mode.py
+++ led_matrix_application/mode/text_mode.py
@@ -1,12 +1,62 @@
 from mode.abstract_mode import AbstractMode
+from RGBMatrixEmulator import graphics
 
 
 class TextMode(AbstractMode):
-    def start(self, matrix):
-        self.matrix = matrix
+    def __init__(self, matrix):
+        super().__init__(matrix)
+        self.font = graphics.Font()
+        self.size = None
+        self.offscreen_canvas = matrix.CreateFrameCanvas()
+
+    def start(self):
+        pass
 
     def update_settings(self, settings):
         self.settings = settings
+        if self.size != self.settings["size"]:
+            self.font.LoadFont(f"fonts/tamzen/{self.settings['size']}.bdf")
+            self.size = self.settings["size"]
 
     def update_display(self):
-        self.matrix.Fill(255, 0, 0)
+        self.offscreen_canvas.Clear()
+
+        one_char_width = self.font.CharacterWidth(0x0020)
+        split_text = self.settings["text"].split(" ")
+
+        line_list = []
+        text_line = ""
+        max_length = 64 // one_char_width
+
+        for i, text in enumerate(split_text):
+            while len(text) > max_length:
+                first_half = text[:max_length]
+                text = text[max_length:]
+                text_line += first_half + " "
+                line_list.append(text_line)
+                text_line = ""
+            if one_char_width * len(text_line + text) < 64:
+                text_line += text + " "
+            else:
+                line_list.append(text_line)
+                text_line = text + " "
+
+        line_list.append(text_line)
+
+        for i, line in enumerate(line_list):
+            trimmed_line = line[:-1]
+            offset_left = 0
+            if self.settings["align"] == "center":
+                width = one_char_width * len(trimmed_line)
+                offset_left = (64 - width) // 2
+
+            graphics.DrawText(
+                self.offscreen_canvas,
+                self.font,
+                offset_left,
+                ((i + 1) * self.font.height) - self.size,
+                graphics.Color(*self.settings["color"]),
+                trimmed_line,
+            )
+
+        self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
