d2522534ed41efd1b90c080cb6be6c6b3b08b631
tsomic <keller.nils99@gmail.com>
2024-08-13 20:15:10 +0200

    ✨ change start.sh to support running on boot

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd led_matrix_application &&
+cd /home/admin/led-matrix-application/led_matrix_application &&
 sudo /home/admin/.cache/pypoetry/virtualenvs/led-matrix-application-eByq54Dv-py3.9/bin/hypercorn -b 0.0.0.0:8080 main:app
