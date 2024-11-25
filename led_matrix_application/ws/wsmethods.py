984fdc7fd4bbd2ac8bba6a553233b10aa4fb5b87
StarAppeal <silkfan11@gmail.com>
2024-11-25 12:26:15 +0100

    small refactoring

--- led_matrix_application/ws/wsmethods.py
+++ led_matrix_application/ws/wsmethods.py
@@ -9,35 +9,21 @@ class WebsocketMethods:
 
     def on_open(self, ws):
         print("Connection opened")
-        ws.send("GET_STATE")
+        self.send_message(ws, {"type": "GET_STATE"})
 
     def on_message(self, ws, message):
         print("Message received")
-
         json_message = json.loads(message)
-
-        #todo: refactoring needed
-        print(json_message)
         print(self.latest_mode)
+        print("Current Time is: ", datetime.now())
+
         if "type" not in json_message:
-            # broadcasted message from server
-            self.led_matrix_controller.update_state(json_message)
-            if self.latest_mode != "music" and json_message["global"]["mode"] == "music":
-                ws.send("GET_SPOTIFY_UPDATES")
-            if self.latest_mode == "music" and json_message["global"]["mode"] != "music":
-                ws.send("STOP_SPOTIFY_UPDATES")
-            self.latest_mode = json_message["global"]["mode"]
-            return
-        if json_message["type"] == "SPOTIFY_UPDATE" and self.latest_mode == "music":
-            print("SPOTIFY_UPDATE")
-            self.led_matrix_controller.modes["music"].update_song_data(json_message["payload"])
+            # Broadcasted message from server
+            self.handle_state_update(ws, json_message)
+        elif json_message["type"] == "SPOTIFY_UPDATE" and self.latest_mode == "music":
+            self.handle_spotify_update(json_message)
         elif json_message["type"] == "STATE":
-            self.led_matrix_controller.update_state(json_message["payload"])
-            if self.latest_mode != "music" and json_message["payload"]["global"]["mode"] == "music":
-                ws.send("GET_SPOTIFY_UPDATES")
-            if self.latest_mode == "music" and json_message["payload"]["global"]["mode"] != "music":
-                ws.send("STOP_SPOTIFY_UPDATES")
-            self.latest_mode = json_message["payload"]["global"]["mode"]
+            self.handle_state_update(ws, json_message["payload"])
 
     def on_error(self, ws, error):
         print("Error: ", error)
@@ -48,3 +34,19 @@ class WebsocketMethods:
     def on_ping(self, ws, ping_status):
         print(datetime.now())
         print("Ping received: " + str(ping_status))
+
+    def send_message(self, ws, message):
+        ws.send(json.dumps(message))
+
+    def handle_state_update(self, ws, state):
+        self.led_matrix_controller.update_state(state)
+        new_mode = state["global"]["mode"]
+        if self.latest_mode != "music" and new_mode == "music":
+            self.send_message(ws, {"type": "GET_SPOTIFY_UPDATES"})
+        elif self.latest_mode == "music" and new_mode != "music":
+            self.send_message(ws, {"type": "STOP_SPOTIFY_UPDATES"})
+        self.latest_mode = new_mode
+
+    def handle_spotify_update(self, message):
+        print("SPOTIFY_UPDATE")
+        self.led_matrix_controller.modes["music"].update_song_data(message["payload"])
