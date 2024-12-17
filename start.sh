bc8391e45560e6e791cc2d54b455e6e443f920f2
StarAppeal <silkfan11@gmail.com>
2024-12-17 06:38:23 +0100

    remove files

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/admin/led-matrix-application/led_matrix_application &&
-sudo /home/admin/.cache/pypoetry/virtualenvs/led-matrix-application-eByq54Dv-py3.9/bin/hypercorn -b 0.0.0.0:8080 main:app
--- stop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo kill -9 $(pgrep -f "hypercorn -b 0.0.0.0:8080 main:app") && sudo kill -9 $(sudo netstat -tulnp | awk '/:8080/ {print $7}' | cut -d/ -f1)
