63abd34817af113810110632038bc656646aa54a
tsomic <keller.nils99@gmail.com>
2025-11-21 23:35:17 +0100

    🏷️ add types to state_manager.py params

--- led_matrix_application/state_manager.py
+++ led_matrix_application/state_manager.py
@@ -1,12 +1,16 @@
+from database import Database
+from led_matrix_controller import LEDMatrixController
+
+
 class StateManager:
-    def __init__(self, database, led_controller):
-        self.db = database
+    def __init__(self, database: Database, led_controller: LEDMatrixController):
+        self.database = database
         self.led_controller = led_controller
-        self.led_controller.update_state(self.db.get_state())
+        self.led_controller.update_state(self.database.get_state())
 
     def get_state(self) -> dict:
-        return self.db.get_state()
+        return self.database.get_state()
 
     def update_state(self, new_data: dict):
-        self.db.patch_state(new_data)
-        self.led_controller.update_state(self.db.get_state())
+        self.database.patch_state(new_data)
+        self.led_controller.update_state(self.database.get_state())
